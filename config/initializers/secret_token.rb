260ebe88cc011779dff16b397c97e984dc476538
Graeme Roche <graemer@gmail.com>
2013-02-13 10:53:14 -0800

    First additions

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pureendurance::Application.config.secret_token = '75147d8316c26d231369426ae31282b77eedfa13f130623c79488faead0a94c29fb228e5ae0966e1aaec81466cd737fc07aa61a2edf8b6e947a88f2fff0b37b2'
